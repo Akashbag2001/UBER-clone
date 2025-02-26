590c130f6331e9edf564b43085255c093f328951
AkashBag2001 <bagakash11@gmail.com>
2025-02-26 18:28:07 +0530

    xcas

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,12 @@
 const dotenv = require('dotenv')
 dotenv.config();
 const cors = require('cors')
-
+const connectToDb = require('./db/db.js')
 const express = require('express');
 const app = express();
 
 app.use(cors());
-
+connectToDb()
 app.get('/',(req,res)=>{
     res.send("Hello World!");
 })
